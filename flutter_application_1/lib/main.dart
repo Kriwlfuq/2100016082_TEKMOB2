ef184c1ee0c44f808a7b604fc1dab726abd97187
Kriwlfuq <fikriariansyah2002@gmail.com>
2024-04-01 14:09:54 +0700

    first commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,38 +7,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Stack"),
+        appBar: AppBar(
+          title: Text("Stack with Button"),
+        ),
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 0,
+                left: 0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your button action here
+                  },
+                  child: Text("Click Me"),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 80,
+                  margin: EdgeInsets.only(top: 20),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.purple,
+                  height: 70,
+                  width: 70,
+                  margin: EdgeInsets.only(top: 20),
+                ),
+              ),
+            ],
           ),
-          body: Center(
-            child: Stack(
-              children: <Widget>[
-                Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      color: Colors.red,
-                      height: 100,
-                      width: 100,
-                    )),
-                Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      color: Colors.blue,
-                      height: 100,
-                      width: 80,
-                      margin: EdgeInsets.only(top: 20),
-                    )),
-                Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      color: Colors.purple,
-                      height: 70,
-                      width: 70,
-                      margin: EdgeInsets.only(top: 20),
-                    ))
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
